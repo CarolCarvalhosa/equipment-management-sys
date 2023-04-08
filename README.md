cdde72ad44b958382038fdc6f7748513c96091b9
carolcarvalhosa <ccscarvalhosa@gmail.com>
2023-04-08 11:24:03 -0300

    readme + fixing dates

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Equipment Management System - Getting Started
 
-This project aims to create a web app that lists equipments based on messages received from them. It uses [RabbitMQ](https://www.rabbitmq.com/) to handle the message queue. Folders description:
+This project aims to create a web app that lists equipments based on messages received from them. It uses [RabbitMQ](https://www.rabbitmq.com/) to handle the message queue.
+
+Folders description:
 
  - api/ : The API consumer service that receives process and stores the messages on SQLite;
  - message-app/ : The ReactJS app that shows the list of equipment based on the filter and also generates a graph with poweron and poweroff equipment tags;
  - send-message-service/ : A producer service to test sending messages to the API;
+
+Technologies chosen:
+
+- fastify: fastify is one of the fastest web frameworks that provides a plugin architecture;
+- amqplib (rabbitMQ library): this message broker was chosen because I needed a middleware specialized in storing, distributing and controlling the processing of messages in a fast and easy way;
+- knex: query builder that simplifies back-end requests and database changes;
+- sqlite3: simple and easy to start, use and manage sql database, I like to use it for small applications;
+- mui: component library to make my development faster;
+- vite: provides a dev server with rich feature enhancements over native ES modules, faster do develop;
+- recharts: recommended react js library that generates charts;
+- yarn: package manager safer, more reliable, and more efficient than npm;
 
 ## Running locally
 
@@ -53,3 +66,30 @@ Use postman to POST more messages. Sample request:
   "timestamp": "2023-04-07T16:34:18-03:00"
 }
 ```
+
+# Screenshots
+
+Screen to display all active equipment (equipment that reported data in the last 30 minutes);
+
+<img src="screenshots/active-equipments.png" /> 
+
+</br>
+
+Screen to display all equipment that is not reporting and with a status indicating the time that a certain equipment does not report data;
+ - 24h without reporting (Warning) - Orange triangle;
+ - more than 24h without reporting (Critical) - Red stop sign with an x;
+
+<img src="screenshots/not-reporting-equipments.png" /> 
+
+</br>
+
+Screen to display a graph showing all connected equipment and all disconnected equipment (equipment that sended poweroff as the last message)
+
+<img src="screenshots/equipments-graph.png" />
+
+</br>
+
+The system must have a screen with suggestions of actions that the operator must perform when receiving equipment fault codes.
+
+<img src="screenshots/bad-configuration-error.png" />
+<img src="screenshots/memory-error.png" />
--- api/src/repositories/MessageRepository.ts
+++ api/src/repositories/MessageRepository.ts
@@ -16,41 +16,41 @@ export class MessageRepository {
    * Get latest messages from active equipments.
    * @description Latest message from each equipment from the last 30 minutes.
    * 
-   * SQL Generated: select * from messages group by IMEI having max(timestamp) >= datetime('now', '-30 minutes', 'localtime');
+   * SQL Generated: select * from messages group by IMEI having max(datetime(timestamp, 'localtime')) >= datetime('now', '-30 minutes', 'localtime');
    * @returns active equipments.
    */
   public async getLatestMessagesFromLast30Minutes(): Promise<Message[]> {
     return await knex('messages')
       .select('*')
       .groupBy('IMEI')
-      .havingRaw('max(timestamp) >= datetime(\'now\', \'-30 minutes\', \'localtime\')');
+      .havingRaw('max(datetime(timestamp, \'localtime\')) >= datetime(\'now\', \'-30 minutes\', \'localtime\')');
   }
 
   /**
    * Get latest messages from idle equipments.
    * @description Latest message from each equipment triggered more than 30 minutes ago with timestamp diff in minutes.
    * 
-   * SQL Generated: select *, round((julianday(datetime('now', 'localtime')) - julianday(timestamp)) * (24 * 60)) as timestamp_minutes_diff from messages group by IMEI having max(timestamp) < datetime('now', '-30 minutes', 'localtime');
+   * SQL Generated: select *, round((julianday(datetime('now', 'localtime')) - julianday(datetime(timestamp, 'localtime'))) * (24 * 60)) as timestamp_minutes_diff from messages group by IMEI having max(datetime(timestamp, 'localtime')) < datetime('now', '-30 minutes', 'localtime');
    * @returns idle equipments.
    */
   public async getLatestMessagesTriggeredLongerThan30MinutesAgoWithMinutesDiff(): Promise<MessageWithDiff[]> {
     return await knex('messages')
       .select('*')
-      .select(knex.raw('round((julianday(datetime(\'now\', \'localtime\')) - julianday(timestamp)) * (24 * 60)) as timestamp_minutes_diff'))
+      .select(knex.raw('round((julianday(datetime(\'now\', \'localtime\')) - julianday(datetime(timestamp, \'localtime\'))) * (24 * 60)) as timestamp_minutes_diff'))
       .groupBy('IMEI')
-      .havingRaw('max(timestamp) < datetime(\'now\', \'-30 minutes\', \'localtime\')');
+      .havingRaw('max(datetime(timestamp, \'localtime\')) < datetime(\'now\', \'-30 minutes\', \'localtime\')');
   }
 
   /**
    * Get count of poweron and poweroff latest messages from equipments.
    * @description Count of poweron and poweroff latest messages.
    * 
-   * SQL Generated: select tag, count(tag) as count from (select IMEI, tag, max(timestamp) as timestamp from messages where tag != 'timebased' group by IMEI) as subQuery group by tag;
+   * SQL Generated: select tag, count(tag) as count from (select IMEI, tag, max(datetime(timestamp, 'localtime')) as timestamp from messages where tag != 'timebased' group by IMEI) as subQuery group by tag;
    * @returns poweron and poweroff messages count.
    */
   public async getLatestMessagesWithPowerOnAndPowerOffTag(): Promise<MessageTagCount[]> {
     const subQuery = knex('messages')
-      .select('IMEI', 'tag', knex.raw('max(timestamp) as timestamp'))
+      .select('IMEI', 'tag', knex.raw('max(datetime(timestamp, \'localtime\')) as timestamp'))
       .whereNot('tag', 'timebased')
       .groupBy('IMEI', 'tag')
       .as('subQuery');
